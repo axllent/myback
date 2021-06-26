6b03775b9dc1a96afeb06b9a3f390e45999f61ea
Ralph Slooten <axllent@gmail.com>
2021-06-26 16:55:31 +1200

    Use logger for error formatting

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -28,7 +27,6 @@ func Execute() {
 	})
 
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
